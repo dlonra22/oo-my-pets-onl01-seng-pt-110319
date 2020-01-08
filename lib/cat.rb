2831aeb821dae13b21d746fae3b981cba0693a4f
arnie b <arnold.bwaila@gmail.com>
2020-01-08 22:10:00 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,11 @@
 class Cat
   # code goes 
-  attr_accessor :name, :owner
+  attr_accessor :owner, :mood
+  attr_reader :name
   
-  def initialize(name, owner)
+  def initialize(name, owner, mood ="nervous")
     @name = name
     @owner = owner
+    @mood = mood
   end
 end
