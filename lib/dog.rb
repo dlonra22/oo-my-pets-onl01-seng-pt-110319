ddb8e53b18c065cb4ffa5e00b9aa6ca8913b3f82
arnie b <arnold.bwaila@gmail.com>
2020-01-08 22:12:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,11 @@ class Dog
   # code goes here
   attr_accessor :owner, :mood
   attr_reader :name
-  
+  @@all = []
   def initialize(name, owner, mood ="nervous")
     @name = name
     @owner = owner
     @mood = mood
+    @@all << self
   end
 end
